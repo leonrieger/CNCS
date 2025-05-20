d9939bee71356c225e561680438f79726adfe87f
Leon <leon.rieger@htl-kapfenberg.at>
2025-05-20 20:08:54 +0200

    removed pugixml

--- sourcecode/DATABASE-CTRL/src/SQLite3.cpp
+++ sourcecode/DATABASE-CTRL/src/SQLite3.cpp
@@ -11,48 +11,50 @@ using namespace sqlite_database;
 //needed for pugixml
 using namespace pugi;
 
-database_file::database_file(string filename) {//void open
-	xml_document configuration;
-	configuration.load_file("config.xml");
-	xml_node root = configuration.child("CONFIGURATION");
-	xml_node paths = root.child("PATHS");
-	string path_gotten_by_xml;
-	for (xml_node path = paths.first_child(); path; ) {
-		xml_node next_path = path.next_sibling();
-		string usage = path.attribute("usage").value();
-		if (usage == string("database_file_storage")) {
-			path_gotten_by_xml = path.child_value();
-			break;
-		}
-		path = next_path;
-	}
-	string fileposition = path_gotten_by_xml + filename;
-	sqlite3_open(fileposition.c_str(), &(this->database_file_pointer));
-}
-
-void database_file::close() {
-	sqlite3_close(database_file::database_file_pointer);
+database_file::database_file(string filename) {
+    xml_document configuration;
+    configuration.load_file("config.xml");
+    xml_node root = configuration.child("CONFIGURATION");
+    xml_node paths = root.child("PATHS");
+    string path_gotten_by_xml;
+    for (xml_node path = paths.first_child(); path; ) {
+        xml_node next_path = path.next_sibling();
+        string usage = path.attribute("usage").value();
+        if (usage == string("database_file_storage")) {
+            path_gotten_by_xml = path.child_value();
+            break;
+        }
+        path = next_path;
+    }
+    string fileposition = path_gotten_by_xml + filename;
+    sqlite3_open(fileposition.c_str(), &(this->database_file_pointer));
 }
 
 database_file::~database_file() {
-	delete(this);
+    delete(this);
+}
+
+void database_file::close() {
+    sqlite3_close(this->database_file_pointer);
 }
 
 //**************************************************************************************
 
 database::database(database_file& in_database_file) {
-	//Copies the database reference
-	this->database_file_pointer = in_database_file.database_file_pointer;
+    //Copies the database reference
+    this->database_file_pointer = in_database_file.database_file_pointer;
 }
 
 template<typename ... ARGS>
 void database::create(string name, ARGS ... args) {
-	//add all db-arguments
-	string arguments = ("" + ... + args);
-	string argChain = "CREATE TABLE " + name + "(ID INT PRIMARY KEY NOT NULL, " + arguments + ");";
+    //add all db-arguments
+    string arguments = ("" + ... + args);
+    string argChain = "CREATE TABLE " + name + "(ID INT PRIMARY KEY NOT NULL, " + arguments + ");";
 
-	//int status = sqlite3_exec();
+    //int status = sqlite3_exec();
+    if (status != SQLITE_OK) {
+        throw databaseError(1021, "Database creation failed due to an SQLite3 error");
+    }
 }
 
 //**************************************************************************************
-//int main(int argc, char* argv[]) <- benutzt für argumente beim aufrufen der cmd z.b programm.exe argument1 ardument2 ...
