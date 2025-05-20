2fa2eff4fb57a9721945c043c1bdd9cdde67eae0
Leon <leon.rieger@htl-kapfenberg.at>
2025-05-20 12:40:22 +0200

    wtf #2

--- sourcecode/DATABASE-CTRL/src/errors/errors.cpp
+++ sourcecode/DATABASE-CTRL/src/errors/errors.cpp
@@ -6,5 +6,7 @@ databaseError::databaseError(uint16_t EID, string errorMessage) {
 
 const char* databaseError::what() const throw() {
 	//format the output a bit
+	errorMessageFormatted = "There occured this database error: >> "+errorMsg+" <<\tFor futher documentation visit: https://github.com/leonrieger/CNCS/blob/main/documentation/errors/database-errors/E%5d.md",errorID;
+
 	return errorMessageFormatted.c_str();
 }
