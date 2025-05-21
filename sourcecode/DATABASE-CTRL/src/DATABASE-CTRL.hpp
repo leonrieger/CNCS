4eab2c0f6349f33b6214fff9ac5f87d107ce6f21
Leon <leon.rieger@htl-kapfenberg.at>
2025-05-21 07:34:31 +0200

    made everything spc

--- sourcecode/DATABASE-CTRL/src/DATABASE-CTRL.hpp
+++ sourcecode/DATABASE-CTRL/src/DATABASE-CTRL.hpp
@@ -19,49 +19,49 @@ using namespace pugi;
 
 namespace sqlite_database {
 
-	class DLLMODE database_file {
-	public:
-		//database_file();
-		database_file(string filename);
-		~database_file();
-		//void open(string filename);
-		void close();
-		//friend class database_template;
-		friend class database;
-	private:
-		string db_file_name;
-		string xml_file_name;
-		xml_document xml_file_parsed;
-		sqlite3* database_file_pointer;
-	};
+    class DLLMODE database_file {
+    public:
+        //database_file();
+        database_file(string filename);
+        ~database_file();
+        //void open(string filename);
+        void close();
+        //friend class database_template;
+        friend class database;
+    private:
+        string db_file_name;
+        string xml_file_name;
+        xml_document xml_file_parsed;
+        sqlite3* database_file_pointer;
+    };
 
 
 
-	//-----------------------------------------------------
-	class DLLMODE database {
-	public:
-		database(database_file& in_database_file);
-		//bool open(char name[]);
-		
-		template<typename ... ARGS>
-		void create(string name, ARGS ... args);
-		//void insert();
-		//void get();
-		//void search();
-	private:
-		sqlite3* database_file_pointer;
-	};
+    //-----------------------------------------------------
+    class DLLMODE database {
+    public:
+        database(database_file& in_database_file);
+        //bool open(char name[]);
+        
+        template<typename ... ARGS>
+        void create(string name, ARGS ... args);
+        //void insert();
+        //void get();
+        //void search();
+    private:
+        sqlite3* database_file_pointer;
+    };
 
-	//-----------------------------------------------------
+    //-----------------------------------------------------
 
-	struct DLLMODE database_configuration_options {
-		string charField(char name[], uint16_t max_lenght, bool null_allowed = false);
-		string integerField(char name[], bool null_allowed = false);
-		string floatingField(char name[], bool null_allowed = false);
-		string textField(char name[], bool null_allowed = false);
-		string booleanField(char name[], bool defaulting_to = false, bool null_allowed = false);
-		string binaryField(char name[], bool null_allowed = false);
-		string emailField(char name[], bool null_allowed = false);
-	} models;
+    struct DLLMODE database_configuration_options {
+        string charField(char name[], uint16_t max_lenght, bool null_allowed = false);
+        string integerField(char name[], bool null_allowed = false);
+        string floatingField(char name[], bool null_allowed = false);
+        string textField(char name[], bool null_allowed = false);
+        string booleanField(char name[], bool defaulting_to = false, bool null_allowed = false);
+        string binaryField(char name[], bool null_allowed = false);
+        string emailField(char name[], bool null_allowed = false);
+    } models;
 
 }
--- sourcecode/TESTS/MATH-Test/main.cpp
+++ sourcecode/TESTS/MATH-Test/main.cpp
@@ -4,12 +4,12 @@ using namespace unit_conversions;
 using namespace std;
 
 int main() {
-	int i = 10;
-	double test = 115.202;
-	cout << "test1" << endl;
-	cout << inch_to_mm(i) << endl;
-	cout << inch_to_mm(test) << endl;
+    int i = 10;
+    double test = 115.202;
+    cout << "test1" << endl;
+    cout << inch_to_mm(i) << endl;
+    cout << inch_to_mm(test) << endl;
 
-	char x[10];
-	cin >> x;
+    char x[10];
+    cin >> x;
 }
