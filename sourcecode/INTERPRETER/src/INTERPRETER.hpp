f55023d70729ab6d190ccc943f29c368da9ddbdc
Leon <leon.rieger@htl-kapfenberg.at>
2025-05-23 10:10:01 +0200

    created command struct

--- sourcecode/INTERPRETER/src/INTERPRETER.hpp
+++ sourcecode/INTERPRETER/src/INTERPRETER.hpp
@@ -2,15 +2,70 @@
 
 #include <stdint.h>
 #include <string.h>
+#include <vector>
 
-class universal_interpreter {
+using namespace std;
 
-};
+namespace interpreter_system {
 
-class gcode_interpreter {
+    class interpreter {
+    public:
 
-};
+    private:
+        uint64_t current_state; //the designated indicator how far the program has progressed
+        Axis_3d currentPosition;
+        Axis_3d machineZeroPoint;
+        Axis_3d workpieceZeroPoint;
+        Axis_3d currentToolOffset;
+        vector<Command> allCommandsArr;
+    };
 
-class mcode_interpreter {
+    class gcode_interpreter {
+    public:
+    private:
+    };
 
-};
+    class mcode_interpreter {
+    public:
+    private:
+    };
+
+    struct Axis_3d {
+        double X;
+        double Y;
+        double Z;
+    };
+    /*
+    struct Axis_6d {
+        double X;
+        double Y;
+        double Z;
+
+        double A;
+        double B;
+        double C;
+    };
+    */
+    struct Command {// currently build for 3d
+        bool gcode;
+        bool mcode;
+        bool direct_command;
+
+        uint16_t command;
+        uint32_t N;
+
+        double I;// Interpolation Parameters
+        double J;
+        double K;
+
+        double X;// Coordinates
+        double Y;
+        double Z;
+
+        double F;// feed (Vorschub)
+
+        uint32_t P;// pause in ms
+
+        uint16_t T;// tool to be selected
+    };
+}
