70c48bc5bcf295610ca696a5ef406974c5b2c066
Leon <leon.rieger@htl-kapfenberg.at>
2025-05-26 12:43:04 +0200

    working on cmd ui

--- sourcecode/CMD/src/main.cpp
+++ sourcecode/CMD/src/main.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
 
-int main( int argc, char* argv[] ) {
+using namespace std;
 
+int main( int argc, char* argv[] ) {
+    if (argc >= 2) {
+        cout << "Testapplication" << "\t#of Args: " << argc << endl;
+        for (int i = 0; i < argc; i++) {
+            cout << "State: " << i << " || " << argv[i] << endl;
+        }
+    }
+    else if (argc <= 1) {
+        char input[1000] = {};
+        cout << "sucess" << endl << ">>> ";
+
+        cin >> input;
+    }
 }
