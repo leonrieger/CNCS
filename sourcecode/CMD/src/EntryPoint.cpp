e624185b8dbe57b11f32d78b5bf0092d9771021e
Leon <leon.rieger@htl-kapfenberg.at>
2025-05-27 13:56:44 +0200

    added init progress bar

--- sourcecode/CMD/src/EntryPoint.cpp
+++ sourcecode/CMD/src/EntryPoint.cpp
@@ -1,11 +1,16 @@
 #include "EntryPoint.hpp"
 using namespace ui;
 
+#include "ui_elements/ui_elements.hpp"
+
 void entrypoint::initExecCommand() {
 
 }
 
 void entrypoint::initExecEnviroment() {
+    ui_elements::progress_bar init_progress_bar("initialising", 5, ui_elements::colors.OKCYAN);
+
+
     bool requestToStop = false;
     do 
     {
