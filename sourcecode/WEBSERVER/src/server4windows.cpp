3c0082f4accdfa9e9c959240a070dba191771385
Leon <leon.rieger@htl-kapfenberg.at>
2025-05-31 17:31:07 +0200

    created server startup procedure

--- sourcecode/WEBSERVER/src/server4windows.cpp
+++ sourcecode/WEBSERVER/src/server4windows.cpp
@@ -5,16 +5,19 @@
 using namespace webserver;
 
 #include <ws2tcpip.h>
+#include <format>
 
-webserver::SERVER::SERVER(webserver::IP_ADDR ip_information) {
+SERVER::SERVER(webserver::IP_ADDR ip_information) {
     server_ip_info = ip_information;
 
-    //client_socket = socket(AF_INET, SOCK_STREAM, 0);
+    client_socket = socket(AF_INET, SOCK_STREAM, 0);
 
     socket_information.sin_family = AF_INET;
     inet_pton(AF_INET, ip_information.getIP().c_str(), &socket_information.sin_addr);
     socket_information.sin_port = htons(ip_information.getPORT());
 
+    server_socket_size = sizeof(socket_information);
+    
     if (WSAStartup(MAKEWORD(2, 2), &server_wsadata) != 0) {
         throw webServerError(1, "");
     }
@@ -26,19 +29,21 @@ webserver::SERVER::SERVER(webserver::IP_ADDR ip_information) {
         WSACleanup();
         throw webServerError(2, "couldn't connect to socket");
     }
-
-    server_socket_size = sizeof(socket_information);
-    if (bind(server_socket, (sockaddr*)&socket_information, server_socket_size) == SOCKET_ERROR) {
-        ostringstream oss;
-        oss << WSAGetLastError(); // Convert the error code to a string
-        WSACleanup();
-        throw webServerError(3, oss.str());
-    }
 }
 
-webserver::SERVER::~SERVER() {
+SERVER::~SERVER() {
     closesocket(server_socket);
 
     WSACleanup();
+}
+
+void SERVER::startup() {
+    if (bind(server_socket, (sockaddr*)&socket_information, server_socket_size) == SOCKET_ERROR) {
+        int32_t lastError = WSAGetLastError();
+        WSACleanup();
+        throw webServerError(3, format("Error: {0} ", lastError));
+    }
+
+
 }
 
