a921b41a6642c6335ca4359a5b3ddcb2c1a5b811
Leon <leon.rieger@htl-kapfenberg.at>
2025-06-01 18:40:42 +0200

    tested both systems --- everything working!

--- sourcecode/WEBCLIENT/src/WEBCLIENT.hpp
+++ sourcecode/WEBCLIENT/src/WEBCLIENT.hpp
@@ -13,16 +13,12 @@ namespace web {
 
         string read();
         void write(string data);
-        void end();
 
     private:
         IP_ADDR server_ip_info;
 
         sockaddr_in server_socket_information;
         SOCKET server_socket;
-        int32_t server_socket_size;
-        WSADATA server_wsadata;
-
         int32_t bytesReceived = 0;
     };
 }
--- sourcecode/WEBCLIENT/src/client4windows.cpp
+++ sourcecode/WEBCLIENT/src/client4windows.cpp
@@ -3,6 +3,7 @@
 using namespace web;
 
 #include <ws2tcpip.h>
+#include <memory>
 
 CLIENT::CLIENT(IP_ADDR ip_information) {
     server_ip_info = ip_information;
@@ -37,13 +38,47 @@ CLIENT::~CLIENT() {
 }
 
 void CLIENT::write(string data) {
-
+    int32_t bytesSent;
+    int32_t totalBytesSent = 0;
+    while (totalBytesSent < data.size()) {
+        bytesSent = send(server_socket, data.c_str(), data.size(), 0);
+        if (bytesSent < 0) {
+            break;
+        }
+        totalBytesSent += bytesSent;
+    }
+    if (totalBytesSent != data.size()) {
+        throw webClientError(4, "Send failed");
+    }
 }
 
 string CLIENT::read() {
+    const size_t BUFFER_SIZE = 30000;
+    vector<char> buffer;
+    unique_ptr<char[]> temp(new char[BUFFER_SIZE]);
+    int32_t received;
 
-}
+    const string END_OF_HEADERS = "\r\n\r\n";
 
-void CLIENT::end() {
+    while (true) {
+        received = recv(server_socket, temp.get(), BUFFER_SIZE, 0);
+        if (received < 0) {
+            throw webServerError(6, "Number of received bytes smaller than one");
+        }
+        if (received == 0) {
+            break;
+        }
+        buffer.insert(buffer.end(), temp.get(), temp.get() + received);
 
+        if (buffer.size() >= END_OF_HEADERS.size()) {
+            std::string buf_str(buffer.data(), buffer.size());
+            if (buf_str.find(END_OF_HEADERS) != std::string::npos) {
+                break;
+            }
+        }
+        if (received < BUFFER_SIZE) {
+            break;
+        }
+    }
+    return string(buffer.data(), buffer.size());
 }
