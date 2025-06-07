393d01a436d7ce9e62f90dd15cbbe5be92965cc0
Leon <leon.rieger@htl-kapfenberg.at>
2025-06-07 16:06:56 +0200

    added serial port detection

---
+++ sourcecode/USB/src/USB_SERIAL/test.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#define WIN32_LEAN_AND_MEAN
+#undef UNICODE
+#include <Windows.h>
+
+bool SelectComPort() //added function to find the present serial 
+{
+    char lpTargetPath[5000]; // buffer to store the path of the COMPORTS
+    bool gotPort = false; // in case the port is not found
+
+    for (int i = 0; i < 255; i++) // checking ports from COM0 to COM255
+    {
+        std::string str = "COM" + std::to_string(i); // converting to COM0, COM1, COM2
+        DWORD test = QueryDosDevice(str.c_str(), lpTargetPath, 5000);
+
+        // Test the return value and error if any
+        if (test != 0) //QueryDosDevice returns zero if it didn't find an object
+        {
+            std::cout << str << ": " << lpTargetPath << std::endl;
+            gotPort = true;
+        }
+
+        if (GetLastError() == ERROR_INSUFFICIENT_BUFFER)
+        {
+        }
+    }
+
+    return gotPort;
+}
+
+int main() {
+    SelectComPort();
+    return 0;
+}
