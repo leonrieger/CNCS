c735a625ccb6b52bba41669161a52dab6038c575
Leon <leon.rieger@htl-kapfenberg.at>
2025-06-07 17:17:07 +0200

    optimized code style

--- sourcecode/USB/src/USB_SERIAL/test.cpp
+++ sourcecode/USB/src/USB_SERIAL/test.cpp
@@ -1,27 +1,29 @@
 #include <iostream>
 #include <string>
+#include <vector>
 #define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
 
-bool SelectComPort() //added function to find the present serial 
-{
-    char lpTargetPath[5000]; // buffer to store the path of the COMPORTS
-    bool gotPort = false; // in case the port is not found
+using namespace std;
+
+vector<uint8_t> SelectComPort() {
+    char lpTargetPath[500];
+    vector<uint8_t> available_ports;
 
     for (int i = 0; i < 255; i++) {
-        std::string str = "COM" + std::to_string(i);
-        DWORD test = QueryDosDeviceA(str.c_str(), lpTargetPath, 5000);
+        string str = "COM" + to_string(i);
+        DWORD test = QueryDosDeviceA(str.c_str(), lpTargetPath, 500);
 
         if (test != 0) {
-            std::cout << str << ": " << lpTargetPath << std::endl;
-            gotPort = true;
+            available_ports.push_back(i);
         }
     }
-
-    return gotPort;
+    return available_ports;
 }
 
 int main() {
-    SelectComPort();
+    for (int port : SelectComPort()) {
+        cout << "COM" << port << endl;
+    }
     return 0;
 }
