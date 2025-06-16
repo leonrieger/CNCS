5c6680becf82a50c8035991c84e927bf89a51bff
Leon <leon.rieger@htl-kapfenberg.at>
2025-06-16 08:00:17 +0200

    added interpreter result

--- sourcecode/INTERPRETER/src/new/interpreter.h
+++ sourcecode/INTERPRETER/src/new/interpreter.h
@@ -6,8 +6,18 @@ namespace CNCS {
     namespace interpreter {
         struct interpreter_configuration {};
 
-        struct interpreter_result {};
+        struct interpreter_result {
+            
+            double X = 0;
+            double Y = 0;
+            double Z = 0;
 
-        interpreter_result line_interpreter(std::string& info);
+            double A = 0;
+            double B = 0;
+            double C = 0;
+
+        };
+
+        interpreter_result line_interpreter(std::string& line);
     } // namespace interpreter
 } // namespace CNCS
