14ec02a520c425efbde453334742d7df7ded9311
Leon <leon.rieger@htl-kapfenberg.at>
2025-06-16 07:53:28 +0200

    working on interpreter

--- sourcecode/INTERPRETER/src/new/interpreter.h
+++ sourcecode/INTERPRETER/src/new/interpreter.h
@@ -1,11 +1,13 @@
 #pragma once
 
+#include <string>
+
 namespace CNCS {
     namespace interpreter {
-        struct interpreter_result {
+        struct interpreter_configuration {};
 
-        };
+        struct interpreter_result {};
 
-        interpreter_core()
-    }
-}
+        interpreter_result line_interpreter(std::string& info);
+    } // namespace interpreter
+} // namespace CNCS
