0a4bd1efbd30bc4db4822272a3aa33579dc97293
Leon <leon.rieger@htl-kapfenberg.at>
2025-06-16 11:06:55 +0200

    working on interpreter

--- sourcecode/INTERPRETER/src/new/interpreter.hpp
+++ sourcecode/INTERPRETER/src/new/interpreter.hpp
@@ -4,23 +4,16 @@
 
 namespace CNCS {
     namespace interpreter {
-        enum currentWorkingPlane {
-            DEFAULT = 0,
-            XY = 1,
-            YZ = 2,
-            XZ = 3,
-        };
-
         union current_interpreter_status {
-            bool current_unit_system;//false for metric / true for imperial
-            int8_t currentplane;// baseed on 'currentWorkingPlane'
-            bool measurementMode;// false absolut, true inkremental
+            bool current_unit_system; // false for metric / true for imperial
+            int8_t currentplane;      // baseed on 'currentWorkingPlane'
+            bool measurementMode;     // false absolut, true inkremental
+            uint64_t prevLineNumber;  // the previos line number
         };
 
         struct interpreter_configuration {};
 
         struct interpreter_result {
-
             double X = 0;
             double Y = 0;
             double Z = 0;
@@ -30,6 +23,31 @@ namespace CNCS {
             double C = 0;
         };
 
-        interpreter_result line_interpreter(current_interpreter_status& status, std::string& line);
-    }
-}
+        interpreter_result line_interpreter(current_interpreter_status& status,
+                                            std::string& line);
+    } // namespace interpreter
+
+    namespace cnc_machine {
+        namespace tools {
+            struct toolID {
+                uint16_t machineID;
+                uint8_t toolClass;
+                uint8_t toolSubClass;
+                uint16_t toolIdentifier;
+                uint16_t toolSubIdentifier;
+            };
+
+            std::string generateToolIdString(toolID& toolidentifier);
+            toolID generateToolIdStruct(std::string& toolidentifier);
+
+        } // namespace tools
+
+        enum currentWorkingPlane {
+            DEFAULT = 0,
+            XY = 1,
+            YZ = 2,
+            XZ = 3,
+        };
+
+    } // namespace cnc_machine
+} // namespace CNCS
