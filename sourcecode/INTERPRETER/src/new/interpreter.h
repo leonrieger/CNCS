bd8fa3dc2797da05ac982523d73ee4219e5b68f9
Leon <leon.rieger@htl-kapfenberg.at>
2025-06-16 08:13:00 +0200

    added current status

--- sourcecode/INTERPRETER/src/new/interpreter.h
+++ sourcecode/INTERPRETER/src/new/interpreter.h
@@ -4,10 +4,12 @@
 
 namespace CNCS {
     namespace interpreter {
+        union current_status {};
+
         struct interpreter_configuration {};
 
         struct interpreter_result {
-            
+
             double X = 0;
             double Y = 0;
             double Z = 0;
@@ -15,9 +17,8 @@ namespace CNCS {
             double A = 0;
             double B = 0;
             double C = 0;
-
         };
 
-        interpreter_result line_interpreter(std::string& line);
+        interpreter_result line_interpreter(current_status& status, std::string& line);
     } // namespace interpreter
 } // namespace CNCS
