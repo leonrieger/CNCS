7ee95b978f42a24f3c288ca07a51961323fd385e
Leon <leon.rieger@htl-kapfenberg.at>
2025-06-17 13:13:21 +0200

    Update gcode_file_interpreter.cpp

--- sourcecode/INTERPRETER/src/new/core/gcode_file_interpreter.cpp
+++ sourcecode/INTERPRETER/src/new/core/gcode_file_interpreter.cpp
@@ -1,10 +1,8 @@
 #include "core_interpreter.hpp"
 
-namespace CNCS::interpreter {
-    void gcode_file_interpreter(
-        current_interpreter_status& currentstate, std::string& file_content,
-        std::vector<interpreted_commands>& cnc_program_commands) {
-        //------------------------------
+void CNCS::interpreter::gcode_file_interpreter(
+    current_interpreter_status& currentstate, std::string& file_content,
+    std::vector<interpreted_commands>& cnc_program_commands) {
+    //------------------------------
 
-    }
-} // namespace CNCS::interpreter
+}
