15b0f33ab011a1293e104d125be9328159f04311
Leon <leon.rieger@htl-kapfenberg.at>
2025-06-17 08:04:50 +0200

    working on interpreter

--- sourcecode/INTERPRETER/src/new/core/core_interpreter.hpp
+++ sourcecode/INTERPRETER/src/new/core/core_interpreter.hpp
@@ -11,24 +11,34 @@ namespace CNCS::interpreter {
     };
 
     struct current_interpreter_status {
-        bool current_unit_system; // false for metric / true for imperial
-        //int8_t currentplane;      // based on 'currentWorkingPlane'
+        bool current_unit_system =
+            false; // false for metric / true for imperial
         currentWorkingPlane currentPlane;
-        bool measurementMode;     // false absolut, true incremental
-        uint64_t prevLineNumber;  // the previous line number
+        bool measurementMode;    // false absolut, true incremental
+        uint64_t prevLineNumber; // the previous line number
     }; // namespace current_interpreter_status
 
-    struct interpreter_configuration {};
+    struct number_of_commands_on_one_line {
+        uint16_t no_of_commands = 0;
+        uint16_t no_of_commands_executed = 0;
+    };
 
-    struct interpreter_result {
+    struct line_interpreter_result {
         double X = 0.0;
         double Y = 0.0;
         double Z = 0.0;
 
-        float A = 0.0f;
-        float B = 0.0f;
-        float C = 0.0f;
+        //float A = 0.0f;
+        //float B = 0.0f;
+        //float C = 0.0f;
+
+        uint16_t S = 0; // Spindle speed
+
+        currentWorkingPlane plane = DEFAULT;
+
+        std::string comment = "";
     };
 
-    interpreter_result line_interpreter(current_interpreter_status& currentstate, std::string& line);
+    void line_interpreter(current_interpreter_status& currentstate,
+                          std::string& line_to_interpret, line_interpreter_result& result);
 } // namespace CNCS::interpreter
