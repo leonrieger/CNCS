cafb003d17841276a62cffef0c219ed5367eb71b
Leon <leon.rieger@htl-kapfenberg.at>
2025-06-18 19:28:34 +0200

    added basic functions for main.cpp

--- sourcecode/INTERPRETER/src/main.cpp
+++ sourcecode/INTERPRETER/src/main.cpp
@@ -1,1 +1,11 @@
 //for test purposes only
+#include "interpreter.hpp"
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+    if (argc <= 1) {
+        std::cout << "not enogh arguments given!" << std::endl;
+        return 1;
+    }
+    return 0;
+}
