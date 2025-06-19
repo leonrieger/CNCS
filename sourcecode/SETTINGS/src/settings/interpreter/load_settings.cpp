8244d9018e0006b63f5f7c6fa3c2154f2e9c63cf
Leon <leon.rieger@htl-kapfenberg.at>
2025-06-19 14:38:55 +0200

    working on settings

---
+++ sourcecode/SETTINGS/src/settings/interpreter/load_settings.cpp
@@ -0,0 +1,11 @@
+#include "../OS.hpp" // for operating system specific properties
+#include "settings.hpp"
+//#include 
+
+bool CNCS::settings::interpreter::load_settings(
+    USER_ENVIRONMENT_SETTINGS& settings) {
+    
+}
+
+//bool CNCS::settings::interpreter::load_settings(
+//    SYSTEM_ENVIRONMENT_SETTINGS& settings) {}
