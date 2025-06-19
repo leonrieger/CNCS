261f4480ce594656b0e8a3696084e751e1f256d7
Leon <leon.rieger@htl-kapfenberg.at>
2025-06-19 16:00:34 +0200

    working on interpreter settings

--- sourcecode/INTERPRETER/src/config/generate_config.cpp
+++ sourcecode/INTERPRETER/src/config/generate_config.cpp
@@ -7,10 +7,11 @@ namespace CNCS::interpreter::config {
     bool force_line_numbers_declared_with_N = false;
     int32_t lines_to_load_while_interpreting = 100;
 } // namespace CNCS::interpreter::config
-*/
+*//*
 bool CNCS::interpreter::settings::generate_config_from_file(std::string filename) {
     if (filename.empty()) {
         return false;
     }
     return true;
 }
+*/
--- sourcecode/SETTINGS/src/interpreter/load_settings.cpp
+++ sourcecode/SETTINGS/src/interpreter/load_settings.cpp
@@ -7,9 +7,17 @@
 
 bool CNCS::settings::interpreter::load_settings(
     USER_ENVIRONMENT_SETTINGS& settings) {
-
+    // load file
     pugi::xml_document settings_file;
-    settings_file.load_file(std::string(USER_SETTINGS_PATH + "settings.xml").c_str());
+    settings_file.load_file(
+        std::string(USER_SETTINGS_PATH + "settings.xml").c_str());
+    // move to correct child
+    pugi::xml_node interpreter_settings =
+        settings_file.child("settings").child("interpreter");
+    settings.allow_lowercase_characters_in_comments =
+        interpreter_settings.child("allow_lowercase_characters_in_comments")
+            .value() == "true";
+
     return 0;
 }
 
