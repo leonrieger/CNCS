2650627908d4710fa9a931e3afd6169c50c3ad46
Leon <leon.rieger@htl-kapfenberg.at>
2025-06-21 20:55:15 +0200

    fixed errors in math

--- sourcecode/MATH/src/percentages.cpp
+++ sourcecode/MATH/src/percentages.cpp
@@ -3,5 +3,5 @@
 template <typename T>
 float CNCS::math::percentages::calculate_percent(T current_value,
                                                  T full_value) {
-    return static_cast<float>((current_value / full_value) * 100f)
+    return static_cast<float>((current_value / full_value) * 100f);
 }
