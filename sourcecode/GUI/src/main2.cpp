25783f621f6e16dfc34063b2d6862cff7ff683c8
Leon <leon.rieger@htl-kapfenberg.at>
2025-06-24 12:25:12 +0200

    nopé

---
+++ sourcecode/GUI/src/main2.cpp
@@ -0,0 +1,19 @@
+#include <wx/wx.h>
+
+class MyApp : public wxApp {
+public:
+    virtual bool OnInit();
+};
+
+class MyFrame : public wxFrame {
+public:
+    MyFrame() : wxFrame(NULL, wxID_ANY, "Hello World") {}
+};
+
+wxIMPLEMENT_APP(MyApp);
+
+bool MyApp::OnInit() {
+    MyFrame* frame = new MyFrame();
+    frame->Show(true);
+    return true;
+}
