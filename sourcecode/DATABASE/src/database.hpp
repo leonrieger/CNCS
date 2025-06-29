94f68b3dd5f7dfe05ff74769997420a90ddcfa46
Leon <leon.rieger@htl-kapfenberg.at>
2025-06-29 16:06:25 +0200

    starting new with database

---
+++ sourcecode/DATABASE/src/database.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+// the database currently uses SQLite 3
+namespace CNCS::database {
+    class DATABASE_FILE {
+    public:
+    private:
+    };
+
+    class DATABASE_TABLE {
+    public:
+    private:
+    };
+} // namespace CNCS::database
