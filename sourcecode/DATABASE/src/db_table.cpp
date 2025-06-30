9fb6b2f235020bd55c89e85f41bb8fa7500a90ba
Leon <leon.rieger@htl-kapfenberg.at>
2025-06-30 20:07:48 +0200

    implementing db sys

--- sourcecode/DATABASE/src/db_table.cpp
+++ sourcecode/DATABASE/src/db_table.cpp
@@ -1,2 +1,10 @@
 #include "database.hpp"
 
+template <typename... Args>
+CNCS::database::DATABASE_TABLE::DATABASE_TABLE(DATABASE_FILE& database,
+    std::string table_name,
+    Args... args) {
+    // handling the database stuff
+    db_file_pointer = database.db_file_pointer;
+    is_db_connected = &database.connected;
+}
