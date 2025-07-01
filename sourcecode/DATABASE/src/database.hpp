2810aa9c12406288ded043c2ae6708f781c7055b
Leon <leon.rieger@htl-kapfenberg.at>
2025-07-01 19:37:14 +0200

    compilers... you love or hate them

--- sourcecode/DATABASE/src/database.hpp
+++ sourcecode/DATABASE/src/database.hpp
@@ -3,6 +3,7 @@
 #include <sqlite3.h>
 #include <string>
 #include <vector>
+#include <iostream>
 
 namespace CNCS::database {
     class DATABASE_FILE {
@@ -21,7 +22,29 @@ namespace CNCS::database {
     public:
         template <typename... Args>
         DATABASE_TABLE(DATABASE_FILE& database, std::string table_name,
-                       const Args&... args);
+                       const Args&... args) {
+            static_assert((std::is_base_of<fields::FIELD, args>::value && ...),
+                          "DATABASE_TABLE init error: not all classes are "
+                          "derived from FIELD!");
+
+            // handling the database stuff
+            db_file_pointer = database.db_file_pointer;
+            is_db_connected = &database.connected;
+            db_table_name = table_name;
+            //---------
+            std::string initial_sql_message =
+                "CREATE TABLE IF NOT EXISTS " + table_name +
+                "(id INTEGER PRIMARY KEY AUTOINCREMENT";
+            //----------
+            (list_of_fields.push_back(args.copy()), ...);
+
+            for (const auto& field : list_of_fields) {
+                initial_sql_message += field->convert_to_sql();
+            }
+
+            initial_sql_message += ");";
+            std::cout << initial_sql_message << std::endl;
+        }
 
     private:
         sqlite3* db_file_pointer = nullptr;
--- sourcecode/DATABASE/src/db_table.cpp
+++ sourcecode/DATABASE/src/db_table.cpp
@@ -1,30 +1,4 @@
 #include "database.hpp"
 #include <iostream>
 
-template <typename... Args>
-CNCS::database::DATABASE_TABLE::DATABASE_TABLE(DATABASE_FILE& database,
-                                               std::string table_name,
-                                               const Args&... args) {
 
-    static_assert(
-        (std::is_base_of<fields::FIELD, args>::value && ...),
-        "DATABASE_TABLE init error: not all classes are derived from FIELD!");
-
-    // handling the database stuff
-    db_file_pointer = database.db_file_pointer;
-    is_db_connected = &database.connected;
-    db_table_name = table_name;
-    //---------
-    std::string initial_sql_message = "CREATE TABLE IF NOT EXISTS " +
-                                      table_name +
-                                      "(id INTEGER PRIMARY KEY AUTOINCREMENT";
-    //----------
-    (list_of_fields.push_back(args.copy()), ...);
-
-    for (const auto& field : list_of_fields) {
-        initial_sql_message += field->convert_to_sql();
-    }
-
-    initial_sql_message += ");";
-    std::cout << initial_sql_message << std::endl;
-}
