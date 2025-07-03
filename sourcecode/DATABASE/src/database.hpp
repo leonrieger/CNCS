191e271c7684666b3ba549f9789fe336893be4ca
Leon <leon.rieger@htl-kapfenberg.at>
2025-07-03 20:21:04 +0200

    seperating files

--- sourcecode/DATABASE/src/database.hpp
+++ sourcecode/DATABASE/src/database.hpp
@@ -10,16 +10,7 @@
 
     //---------------------------------
 
-    class DATABASE_CONTENT {
-    public:
-        DATABASE_CONTENT(
-            std::string name,
-            std::vector<std::unique_ptr<fields::FIELD>>& list_of_all_fields,
-            std::map<std::string, DB_RETURN_TYPES> database_return);
 
-    private:
-        std::map<std::string, DB_RETURN_TYPES> database_params;
-    };
 
     //---------------------------------
 
--- sourcecode/DATABASE/src/database_content/db_content.hpp
+++ sourcecode/DATABASE/src/database_content/db_content.hpp
@@ -1,1 +1,21 @@
 #pragma once
+
+#include "../fields/fields.hpp"
+
+#include <map>
+#include <string>
+#include <vector>
+#include <memory>
+
+namespace CNCS::database {
+    class DATABASE_CONTENT {
+    public:
+        DATABASE_CONTENT(
+            std::string name,
+            std::vector<std::unique_ptr<fields::FIELD>>& list_of_all_fields,
+            std::map<std::string, DB_RETURN_TYPES> database_return);
+
+    private:
+        std::map<std::string, DB_RETURN_TYPES> database_params;
+    };
+} // namespace CNCS::database
