a2c025d6b101e6d7e13d7af78cf6e57d6a7adcdf
Leon <leon.rieger@htl-kapfenberg.at>
2025-07-03 20:30:11 +0200

    adding more cases to main

--- sourcecode/DATABASE/src/main.cpp
+++ sourcecode/DATABASE/src/main.cpp
@@ -5,11 +5,18 @@ int main() {
     db_file.connect("test.db");
 
     CNCS::database::DATABASE_TABLE test_table(
-        db_file, "test0", CNCS::database::fields::idField(),
+        db_file, "Test", CNCS::database::fields::idField(),
         CNCS::database::fields::integerField("test1"),
         CNCS::database::fields::integerField("test2"),
         CNCS::database::fields::integerField("test3"),
-        CNCS::database::fields::integerField("allajaklak"));
+        CNCS::database::fields::integerField("test4"));
+
+    CNCS::database::DATABASE_TABLE test_table2(
+        db_file, "Test", CNCS::database::fields::idField(),
+        CNCS::database::fields::integerField("a"),
+        CNCS::database::fields::integerField("b"),
+        CNCS::database::fields::integerField("c"),
+        CNCS::database::fields::integerField("d"));
 
     return 0;
 }
