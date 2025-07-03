a845432adb9e79d775e36e7ffc98a7bbc9356699
Leon <leon.rieger@htl-kapfenberg.at>
2025-07-03 20:18:42 +0200

    seperating files

--- sourcecode/DATABASE/src/database.hpp
+++ sourcecode/DATABASE/src/database.hpp
@@ -6,18 +6,7 @@
 #include <string>
 #include <vector>
 
-namespace CNCS::database {
-    class DATABASE_FILE {
-    public:
-        bool connect(const std::string database_name);
-        ~DATABASE_FILE();
 
-        friend class DATABASE_TABLE;
-
-    private:
-        sqlite3* db_file_pointer = nullptr;
-        int8_t connected = 0;
-    };
 
     //---------------------------------
 
--- sourcecode/DATABASE/src/database_table/db_table.cpp
+++ sourcecode/DATABASE/src/database_table/db_table.cpp
@@ -1,2 +1,3 @@
 #include "database.hpp"
 
+
