c9c1e137752b82b2c86a1e6ce4a5935c056609e0
Leon <leon.rieger@htl-kapfenberg.at>
2025-07-03 20:22:24 +0200

    file seperation (ongoing)

--- sourcecode/DATABASE/src/database_table/db_table.hpp
+++ sourcecode/DATABASE/src/database_table/db_table.hpp
@@ -1,3 +1,47 @@
 #pragma once
 
 
+
+namespace CNCS::database {
+    class DATABASE_TABLE {
+    public:
+        template <typename... Args>
+        DATABASE_TABLE(DATABASE_FILE& database, std::string table_name,
+                       const Args&... args) {
+            static_assert((std::is_base_of<fields::FIELD, Args>::value && ...),
+                          "DATABASE_TABLE init error: not all classes are "
+                          "derived from FIELD!");
+
+            // handling the database stuff
+            db_file_pointer = database.db_file_pointer;
+            is_db_connected = &database.connected;
+            db_table_name = table_name;
+            //---------
+            std::string initial_sql_message =
+                std::format("CREATE TABLE IF NOT EXISTS {0} (", table_name);
+            //----------
+            (list_of_fields.push_back(args.copy()), ...);
+
+            for (const auto& field : list_of_fields) {
+                initial_sql_message += field->convert_to_sql();
+            }
+
+            initial_sql_message += ");";
+            sqlite3_exec(db_file_pointer, initial_sql_message.c_str(),
+                         getter_callback, nullptr, &sql_errorMessage);
+        }
+
+        DATABASE_CONTENT get_all_elements();
+
+    private:
+        std::vector<std::map<std::string, DB_RETURN_TYPES>> db_content = {};
+
+        sqlite3* db_file_pointer = nullptr;
+        int8_t* is_db_connected = nullptr;
+        std::vector<std::unique_ptr<fields::FIELD>> list_of_fields = {};
+        std::string db_table_name = "";
+        char* sql_errorMessage = nullptr;
+        void getter_callback(void* NotUsed, int argc, char** argv,
+                             char** columnName);
+    };
+} // namespace CNCS::database
