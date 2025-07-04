68cbd7fc0d1f4e70fd3ea2bc50eacc8ce91f9221
Leon <leon.rieger@htl-kapfenberg.at>
2025-07-04 17:11:11 +0200

    working on db_table

--- sourcecode/DATABASE/src/database_table_and_file/db_table.cpp
+++ sourcecode/DATABASE/src/database_table_and_file/db_table.cpp
@@ -1,9 +1,15 @@
 #include "db_table_and_file.hpp"
 
+#include <iostream>
+
 int CNCS::database::DATABASE_TABLE::getter_callback(void* this_class_ptr,
                                                     int argc, char** argv,
                                                     char** columnName) {
     auto self = static_cast<DATABASE_TABLE*>(this_class_ptr);
     
+    for (uint32_t i = 0; i < argc; ++i) {
+        std::cout << columnName[i] << " : " << argv[i] << std::endl;
+    }
+
     return 0;
 }
