dc62ce921ced7b55c6b39479e3faf5c10ddefdb6
Leon <leon.rieger@htl-kapfenberg.at>
2025-07-07 14:06:11 +0200

    working on version system

---
+++ sourcecode/SETTINGS/src/version/version.cpp
@@ -0,0 +1,39 @@
+#include "version.hpp"
+
+#include <sstream>
+
+CNCS::settings::VERSION
+CNCS::settings::generate_version(std::string input_version_str) {}
+
+CNCS::settings::VERSION
+CNCS::settings::generate_version(uint32_t input_version_id) {
+    uint16_t v_major = (input_version_id >> 16) & 0xFFFF;
+    uint8_t v_minor = (input_version_id >> 8) & 0xFF;
+    uint8_t v_patch = input_version_id & 0xFF;
+
+    return VERSION(v_major, v_minor, v_patch);
+}
+
+uint32_t CNCS::settings::generate_version_int(VERSION& version) {
+    uint32_t output = 0;
+    output |= (static_cast<uint32_t>(version.major) << 16);
+    // output |= (version.major << 16);
+    output |= (static_cast<uint32_t>(version.minor) << 8);
+    output |= (static_cast<uint32_t>(version.patch));
+
+    return output;
+}
+
+std::string
+CNCS::settings::generate_version_str(VERSION& version,
+                                     bool output_with_v_prefix = false) {
+    std::stringstream output;
+
+    if (output_with_v_prefix) {
+        output << "v";
+    }
+
+    output << version.major << "." << version.minor << "." << version.patch;
+
+    return output.str();
+}
--- sourcecode/SETTINGS/src/version/version.hpp
+++ sourcecode/SETTINGS/src/version/version.hpp
@@ -2,17 +2,25 @@
 
 #include <array>
 #include <stdint.h>
+#include <string>
 
 namespace CNCS::settings {
     struct VERSION {
-        VERSION(const uint8_t version_major, const uint8_t version_minor,
+        VERSION(const uint16_t version_major, const uint8_t version_minor,
                 const uint8_t version_patch)
             : major(version_major), minor(version_minor),
               patch(version_patch) {};
+
         const uint8_t major;
         const uint8_t minor;
         const uint8_t patch;
     };
+
+    VERSION generate_version(std::string input_version_str);
+    VERSION generate_version(uint32_t input_version_id);
+    uint32_t generate_version_int(VERSION& version);
+    std::string generate_version_str(VERSION& version,
+                                     bool output_with_v_prefix = false);
 
     const static VERSION CURRENT_VERSION = VERSION(0, 1, 0);
 
