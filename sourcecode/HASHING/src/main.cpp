aef9f4eb50d1f6cce2423ec43a728e6cd4bcc1bb
Leon <leon.rieger@htl-kapfenberg.at>
2025-07-08 19:19:22 +0200

    adding sha256

---
+++ sourcecode/HASHING/src/main.cpp
@@ -0,0 +1,5 @@
+#include "sha256/sha256.hpp"
+
+int main() {
+    CNCS::cryptography::SHA256 sha;
+    return 0; }
--- sourcecode/HASHING/src/sha256/sha256.hpp
+++ sourcecode/HASHING/src/sha256/sha256.hpp
@@ -1,1 +1,17 @@
 #pragma once
+
+#include <string>
+
+namespace CNCS::cryptography {
+    class SHA256 {
+    public:
+        SHA256();
+
+        //void encrypt(std::string& msg_to_encrypt, std::string& result);
+
+        //~SHA256();
+
+    private:
+        //static bool is_already_initialised;
+    };
+}
